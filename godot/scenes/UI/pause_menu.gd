4fbf79c27ca380a966f0b1992e03a903cbfee91d
Zach Delgros <68976965+Raisinaut@users.noreply.github.com>
2025-03-21 21:18:30 -0700

    Pause button (space or esc) + emit action signal on resume

--- godot/scenes/UI/pause_menu.gd
+++ godot/scenes/UI/pause_menu.gd
@@ -1,1 +1,29 @@
 extends Control
+
+signal action_selected(action)
+
+@onready var action_list = $MarginContainer/VBoxContainer/ActionList
+@onready var resume_button = $MarginContainer/VBoxContainer/Resume
+
+var is_paused = false : set = set_paused
+
+
+func _ready() -> void:
+	set_paused(true)
+	resume_button.pressed.connect(set_paused.bind(false))
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		set_paused(not is_paused)
+
+
+func set_paused(state : bool):
+	is_paused = state
+	get_tree().paused = is_paused
+	visible = is_paused
+	print("pause")
+	
+	if not is_paused:
+		var action : String = action_list.get_selected_action().to_lower()
+		action_selected.emit(action)

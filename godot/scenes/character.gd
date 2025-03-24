7616aa0053283d1c0c8af49a990135d49b6e0039
Jasper Andrew <9101110+jasperandrew@users.noreply.github.com>
2025-03-23 22:18:35 -0400

    simplify level build process

--- godot/scenes/character.gd
+++ godot/scenes/character.gd
@@ -1,20 +1,20 @@
 class_name Character
 extends CharacterBody2D
 
-enum InputAction { NONE, JUMP, CROUCH }
+enum Action { NONE, JUMP, CROUCH }
 
 signal hit_hazard
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-var controller_state := InputAction.NONE
+var controller_state := Action.NONE
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if controller_state == InputAction.JUMP and is_on_floor():
+	if controller_state == Action.JUMP and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	velocity.x = SPEED
@@ -22,17 +22,19 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _process(_delta: float) -> void:
-	if controller_state == InputAction.CROUCH:
+	if controller_state == Action.CROUCH:
 		scale.y = .5
 	else:
 		scale.y = 1
 	
 	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		if collision.get_collider().is_in_group("hazard"):
-			hit_hazard.emit()
-		if collision.get_collider().is_in_group("goal"):
-			hit_hazard.emit()
+		var collider = get_slide_collision(i).get_collider()
+		if not collider is LevelGeometry:
+			continue
+
+		match collider.collision_type:
+			LevelGeometry.Type.HAZARD: hit_hazard.emit()
+			LevelGeometry.Type.GOAL:   hit_hazard.emit()
 
 func _on_pause_menu_action_selected(action:String) -> void:
-	controller_state = InputAction.get(action)
+	controller_state = Action.get(action)
--- godot/scenes/level_geometry.gd
+++ godot/scenes/level_geometry.gd
@@ -1,5 +1,21 @@
+class_name LevelGeometry
 extends StaticBody2D
 
+enum Type { NEUTRAL, HAZARD, GOAL }
+@export var collision_type := Type.NEUTRAL
+
 func _ready() -> void:
-	$ColorRect.position = $CollisionShape2D.position - ($CollisionShape2D.shape.size / 2)
-	$ColorRect.size = $CollisionShape2D.shape.size
+	for child in get_children():
+		if not child is CollisionShape2D:
+			continue
+		
+		var crect = ColorRect.new()
+		crect.position = child.position - (child.shape.size / 2)
+		crect.size = child.shape.size
+
+		match collision_type:
+			Type.NEUTRAL: crect.color = Color("7682d3")
+			Type.HAZARD:  crect.color = Color("bc4c39")
+			Type.GOAL:    crect.color = Color("3e8b5d")
+
+		add_child(crect)
